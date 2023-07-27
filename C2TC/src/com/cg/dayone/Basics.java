732dc947440e43989481d3be95a754151ddfe5d5
oviya <oviya@LAPTOP-UC1L4LMS>
2023-07-27 10:42:09 +0530

    day one commit

---
+++ C2TC/src/com/cg/dayone/Basics.java
@@ -0,0 +1,17 @@
+package com.cg.dayone;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		System.out.println(a+b);//30
+		System.out.println(a-b);//-10
+		System.out.println(a++);//10
+		System.out.println(a%b);//11
+		System.out.println(a/b);//0
+		System.out.println(--b);//19
+	}
+
+}
