8d21664c8ba4a9d2a41693c25ed00365012328cc
oviya <oviya@LAPTOP-UC1L4LMS>
2023-07-30 23:40:00 +0530

    day three

---
+++ C2TC/src/com/cg/daythree/Constructorclass.java
@@ -0,0 +1,12 @@
+package com.cg.daythree;
+
+public class Constructorclass {
+	Constructorclass(){
+		System.out.println("constructor");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Constructorclass cs=new Constructorclass();
+	}
+
+}
---
+++ C2TC/src/com/cg/daythree/Encapsulation.java
@@ -0,0 +1,14 @@
+package com.cg.daythree;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsule e=new Encapsule();
+		e.setName("oviya");
+		e.setId(1);
+		e.setNum(87489374);
+		System.out.println(e.toString());
+	}
+
+}
